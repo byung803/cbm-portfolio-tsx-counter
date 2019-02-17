cd17be6cd3e79d8cd92786d66791391e59535930
BYUNGMIN CHOI <byung803@gmail.com>
2019-02-17 21:46:00 +0900

    tsx counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-class App extends Component {
-  render() {
+import CounterOutput from "./CounterOutput";
+
+// interface IAppProps {}
+
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = {
+    counterValue: 0
+  };
+
+  public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ textAlign: "center" }}>
+        <CounterOutput counter={this.state.counterValue} />
+        <button onClick={this.incHandler}>Increment</button>
+        <button onClick={this.decHandler}>Decrement</button>
       </div>
     );
   }
+
+  private incHandler = () => {
+    this.setState(prevState => ({
+      counterValue: prevState.counterValue + 1
+    }));
+  };
+
+  private decHandler = () => {
+    this.setState(prevState => ({
+      counterValue: prevState.counterValue - 1
+    }));
+  };
 }
 
 export default App;
